b8c104a293e71b263e8b02c0e0a7bb2314e6d2ca
Justin Lee <justin.r.lee@gmail.com>
2025-01-08 14:32:15 +0000

    add local test

--- internal/service/ec2/vpc_network_interface_permission_test.go
+++ internal/service/ec2/vpc_network_interface_permission_test.go
@@ -83,8 +83,8 @@ func TestAccVPCNetworkInterfacePermission_ownerExpectError(t *testing.T) {
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() {
-			acctest.PreCheck(ctx, t) 
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
 		},
 		ErrorCheck:               acctest.ErrorCheck(t, names.EC2ServiceID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
@@ -210,6 +210,108 @@ data "aws_caller_identity" "test" {}
 resource "aws_network_interface_permission" "test" {
     network_interface_id = aws_network_interface.test.id
     account_id           = data.aws_caller_identity.test.account_id
+    permission           = "INSTANCE-ATTACH"
+}
+`, rName))
+}
+
+func TestAccVPCNetworkInterfacePermission_basic_l(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_network_interface_permission.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			// acctest.PreCheckAlternateAccount(t)
+		},
+		ErrorCheck: acctest.ErrorCheck(t, names.EC2ServiceID),
+		// ProtoV5ProviderFactories: acctest.ProtoV5FactoriesAlternate(ctx, t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCNetworkInterfacePermissionDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCNetworkInterfacePermissionConfig_basic_l(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccVPCNetworkInterfacePermissionExists(ctx, resourceName),
+					resource.TestCheckResourceAttrSet(resourceName, names.AttrAccountID),
+					resource.TestCheckResourceAttrSet(resourceName, names.AttrNetworkInterfaceID),
+					resource.TestCheckResourceAttrSet(resourceName, "permission"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{names.AttrSkipDestroy},
+			},
+		},
+	})
+}
+
+func TestAccVPCNetworkInterfacePermission_disappears_l(t *testing.T) {
+	ctx := acctest.Context(t)
+	resourceName := "aws_network_interface_permission.test"
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			// acctest.PreCheckAlternateAccount(t)
+		},
+		ErrorCheck: acctest.ErrorCheck(t, names.EC2ServiceID),
+		// ProtoV5ProviderFactories: acctest.ProtoV5FactoriesAlternate(ctx, t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckVPCNetworkInterfacePermissionDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCNetworkInterfacePermissionConfig_basic_l(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccVPCNetworkInterfacePermissionExists(ctx, resourceName),
+					acctest.CheckResourceDisappears(ctx, acctest.Provider, tfec2.ResourceNetworkInterfacePermission(), resourceName),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccVPCNetworkInterfacePermissionConfig_basic_l(rName string) string {
+	return acctest.ConfigCompose(
+		// acctest.ConfigAlternateAccountProvider(),
+		acctest.ConfigAvailableAZsNoOptIn(),
+		fmt.Sprintf(`
+resource "aws_vpc" "test" {
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_subnet" "test" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_network_interface" "test" {
+  subnet_id = aws_subnet.test.id
+}
+
+// data "aws_caller_identity" "test" {
+//   provider = "awsalternate"
+// }
+
+resource "aws_network_interface_permission" "test" {
+    network_interface_id = aws_network_interface.test.id
+    account_id           = "12345"
+    // account_id           = data.aws_caller_identity.test.account_id
     permission           = "INSTANCE-ATTACH"
 }
 `, rName))
